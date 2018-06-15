d0b87b55aea95159437d4883c6c18e8399dc6634
xuhaoran <xuhaoran416518@gmail.com>
2018-06-15 14:21:43 +0800

    cellkit就基本使用结束

---
+++ CellKitDemo/CellKitDemo/SwipeCell.swift
@@ -0,0 +1,25 @@
+//
+//  SwipeCell.swift
+//  CellKitDemo
+//
+//  Created by haoran on 2018/6/15.
+//  Copyright © 2018年 haoran. All rights reserved.
+//
+
+import UIKit
+import SwipeCellKit
+
+class SwipeCell: SwipeTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- CellKitDemo/CellKitDemo/ViewController.swift
+++ CellKitDemo/CellKitDemo/ViewController.swift
@@ -7,21 +7,80 @@
 //
 
 import UIKit
+import SwipeCellKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var count = 30;
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(SwipeCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(UINib.init(nibName: String(describing: SwipeCell.classForCoder()), bundle: nil), forCellReuseIdentifier: "cell")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    
+    
+    
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return count
     }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SwipeCell
+        
+        cell.delegate = self
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 40))
+        view.backgroundColor = UIColor.darkGray
+        return view // I tried returning view.contentView, it didn't help
+    }
+    
+    //  to support editing the table view.
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.beginUpdates()
+            count -= 1
+            tableView.deleteRows(at: [indexPath], with: .left)
+            tableView.endUpdates()
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+}
 
 
+extension ViewController: SwipeTableViewCellDelegate {
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        // 指定右侧
+        guard orientation == .right else {
+            return nil
+        }
+        
+        let action = SwipeAction.init(style: .default, title: "delete") { (action, index) in
+            tableView.beginUpdates()
+            print("点我删除 第\(indexPath.row)行")
+            self.count -= 1
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.endUpdates()
+        }
+        
+//        action.image
+        return [action]
+    }
+    
+    
 }
 
