b54eb4db3c83e619f192ed862395eabb94d9e60b
xuhaoran <xuhaoran416518@gmail.com>
2018-06-15 11:20:38 +0800

    准备cellkit

---
+++ CellKitDemo/CellKitDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CellKitDemo
+//
+//  Created by haoran on 2018/6/15.
+//  Copyright © 2018年 haoran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
