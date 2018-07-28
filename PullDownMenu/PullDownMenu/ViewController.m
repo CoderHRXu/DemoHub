97081129347eb43050d5bb9fbb022eeed0b4b471
xuhaoran <xuhaoran416518@gmail.com>
2018-07-28 13:40:02 +0800

    修改 topview

--- PullDownMenu/PullDownMenu/ViewController.m
+++ PullDownMenu/PullDownMenu/ViewController.m
@@ -8,7 +8,16 @@
 
 #import "ViewController.h"
 
+#import "Masonry.h"
+//#import "UIColor+YYAdd.h"
+
 @interface ViewController ()
+
+/** 顶部view  */
+@property (nonatomic, strong) UIView *topView;
+/** 提示view  */
+@property (nonatomic, strong) UIView *tipView;
+
 
 @end
 
@@ -16,14 +25,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self layout];
+
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)layout{
+    
+    [self.view addSubview:self.topView];
+    [self.topView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view);
+        make.left.equalTo(self.view);
+        make.right.equalTo(self.view);
+        make.height.mas_equalTo(50);
+    }];
 }
 
-
+- (UIView *)topView{
+    if (!_topView) {
+        _topView                    = [[UIView alloc] init];
+        _topView.backgroundColor    = [UIColor grayColor];
+        UILabel *label              = [[UILabel alloc] init];
+        label.text                  = @"按";
+        label.font                  = [UIFont fontWithName:@"PingHei-Text" size:14];
+//        label.textColor             = [UIColor colorWithHexString:@"333333"];
+        [_topView addSubview:label];
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(self->_topView).offset(20);
+            make.centerY.equalTo(self->_topView);
+        }];
+        UIButton *btn               = [UIButton buttonWithType:UIButtonTypeCustom];
+        [btn setTitle:@"得分率" forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        btn.titleLabel.font         = [UIFont systemFontOfSize:14];
+        btn.layer.cornerRadius      = 2;
+        btn.layer.masksToBounds     = YES;
+//        btn.layer.borderColor       = [UIColor colorWithHexString:@"#D3DCE6"].CGColor;
+        btn.layer.borderWidth       = 2;
+        [_topView addSubview:btn];
+        [btn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(label.mas_right).offset(5);
+            make.centerY.equalTo(label.mas_centerY);
+            make.height.mas_equalTo(26);
+            make.width.mas_equalTo(90);
+        }];
+        UILabel *rightLabel         = [[UILabel alloc] init];
+        rightLabel.text             = @"排列";
+//        rightLabel.textColor        = [UIColor colorWithHexString:@"333333"];
+        rightLabel.font             = [UIFont systemFontOfSize:14];
+        [_topView addSubview:rightLabel];
+        [rightLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(btn.mas_right).offset(5);
+            make.centerY.equalTo(btn);
+        }];
+    }
+    return _topView;
+}
 @end
