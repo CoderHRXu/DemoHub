27d147d72151cbb0f8319cd5e4857664c629b6e9
xuhaoran <xuhaoran416518@gmail.com>
2018-09-17 20:52:05 +0800

    11

--- 前端学习/js/01.vueJS.js
+++ 前端学习/js/01.vueJS.js
@@ -4,12 +4,42 @@
 
 var app = new Vue({
 
-    el:"#app",
-    data:{
-        seen:true,
-        message : 'Hello Vue!'
+    el: "#app",
+    data: {
+        seen: true,
+        message: 'Hello Vue!',
+        site: "vue learn",
+        url: "http://www.akpop.top",
+        alexa: "10000"
+
+    },
+    methods: {
+
+        details: function () {
+            return this.state + ' - 开始正式学习了'
+        }
     }
 })
 
 app.message = 'message is changed';
-// app.seen = false;
+
+// // app.seen = false;
+//
+// var vm = new Vue({
+//
+//     el:"#vue_det",
+//     data:{
+//         seen:true,
+//         message : 'Hello Vue!',
+//
+//         site : "vue learn",
+//         url: "http://www.akpop.top",
+//         alexa: "10000"
+//
+//     },
+//
+//     methods: {
+//             return this.state + ' - 开始正式学习了'
+//         }
+//     }
+// )
