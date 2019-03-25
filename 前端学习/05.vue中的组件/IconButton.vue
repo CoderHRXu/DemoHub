d0ef931661a5a2ac9e924554edcc28c8207261c4
xuhaoran <xuhaoran416518@gmail.com>
2019-03-25 16:16:00 +0800

    iconbutton 左右 上传

---
+++ 前端学习/05.vue中的组件/IconButton.vue
@@ -0,0 +1,47 @@
+<!--
+xxxxxxxxx(描述)
+路由：
+
+@Author : haoran 
+@DATE : 2019-03-22
+-->
+<style scoped lang="stylus">
+
+</style>
+
+<template>
+  <div>
+    组件组件
+    <i class="icon"></i>
+    <span class="buttonText">{{text}}</span>
+    <span>这是组件</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "IconButton",
+
+    data () {
+      return {}
+    },
+    computed: {},
+    mounted () {
+    },
+    methods: {
+      /********* 网络请求 *********/
+
+      /********* 私有方法 *********/
+    },
+    props: {
+      iconName:{
+        type: String
+      },
+
+      text:{
+        type: String,
+        default : ''
+      }
+    }
+  }
+</script>
